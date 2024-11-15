a59e122558e6b240a542a8a618fae777406ef956
ANKIT SRIVASTAVA <ankit.stallian@gmail.com>
2024-11-15 19:22:37 +0530

    app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static("public"))//to store resources on server
 app.use(cookieParser()) 
 
 //Routes import
-import books from "./routes/books.routes.js"
+import books from "./routes/books.routes.js" 
 
 //Routes declarration
 app.use("api/vi/books", books)
