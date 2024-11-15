f4ab361ed134f65448b1d29f0aa387280f2e1ea7
ANKIT SRIVASTAVA <ankit.stallian@gmail.com>
2024-11-15 19:14:12 +0530

    DB connection with server established

---
+++ app.js
@@ -0,0 +1,25 @@
+import express, { urlencoded } from "express"
+import cors from "cors"
+
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}))
+
+// configuring, what type of date we can get and other configurations.
+app.use(express.json({limit: "20kb"}))//json data from frontend
+app.use(express.urlencoded({extended: true, limit: "20kb"}))//data from url
+app.use(express.static("public"))//to store resources on server 
+app.use(cookieParser()) 
+
+//Routes import
+import books from "./routes/books.routes"
+
+//Routes declarration
+app.use("api/vi/books", books)
+
+
+
+export { app } 
