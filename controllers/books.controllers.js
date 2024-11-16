8da4e089fe70da555eb32b2fcf3d65f60f6f9d06
ANKIT SRIVASTAVA <ankit.stallian@gmail.com>
2024-11-16 17:28:28 +0530

    book controllers est.

---
+++ controllers/books.controllers.js
@@ -0,0 +1,30 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import { Book } from "../models/book.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+
+
+
+
+
+
+
+export const getBook = asyncHandler( async (req, res) => {
+
+})
+
+export const getBooks = asyncHandler( async (req, res) => {
+
+})
+export const createBook = asyncHandler( async (req, res) => {
+
+})
+
+export const deleteBook = asyncHandler( async (req, res) => {
+
+})
+
+export const updateBook = asyncHandler( async (req, res) => {
+
+})
