4a1ace17e4cf2c08a98f0cfedfc1b72996c155af
ANKIT SRIVASTAVA <ankit.stallian@gmail.com>
2024-11-16 17:10:13 +0530

    added utility

---
+++ utils/asyncHandler.js
@@ -0,0 +1,13 @@
+/*
+Promise based approach
+*/
+const asyncHandler = (requestHandler) => {
+    //Accepting the method.
+    //Returning the method. 
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+export { asyncHandler }
