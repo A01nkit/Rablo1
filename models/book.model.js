e908f2a83b03d5bab51462ae629c2e5b824363a1
ANKIT SRIVASTAVA <ankit.stallian@gmail.com>
2024-11-16 17:18:21 +0530

    book model created

---
+++ models/book.model.js
@@ -0,0 +1,39 @@
+import mongoose, {Schema} from "mongoose";
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    edition: {
+        type: Number,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "Author"
+    },
+    softCopy: {
+        type: String,   //Cloudinary or AWS S3 bucket url
+        required: true
+    },
+    bookCover: {
+        type: String,   //Cloudinary or AWS S3 bucket url
+        required: true  
+    }
+},
+{
+    timestamps: true
+})
+
+
+export const Book = mongoose.model("Book", bookSchema)
