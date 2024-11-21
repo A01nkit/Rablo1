6ab5c23839284f4128958288c16861fa5cf657fa
ANKIT SRIVASTAVA <ankit.stallian@gmail.com>
2024-11-21 19:15:33 +0530

    User model and logic created

---
+++ controllers/user.controllers.js
@@ -0,0 +1,102 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { User } from "../models/user.model";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+
+export const getUser = asyncHandler( async (req, res) => {
+    const userid = req.params['id']
+    const existedUser = await User.findOne({
+        $or: [{userid}]
+    })
+
+    if(!existedUser) {
+        throw new ApiError(409, "User do not exist. hence, no operation can be done")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, existedUser, "User is available and you got it")
+    )
+})
+
+export const getUsers = asyncHandler( async (req, res) => {
+    const users = await User.find({})
+
+    if(!users) {
+        throw new ApiError(409, "No user available")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, users, "Users are available and you got it")
+    )
+})
+
+export const createUser = asyncHandler( async (req, res) => {
+    //Getting user details and validating
+    const {userid, firstname, secondname} = req.body
+    if (
+        [userid, firstname, secondname].some((field) => 
+        field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All fields are required")
+    }
+
+    //Check if user existed
+    const existedUser= await User.findOne({
+        $or: [{userid}]
+    })
+    if(existedUser) {
+        throw new ApiError(409, `user with userid: ${userid} exist`)
+    }
+
+    //Create user object- creating entry call in db
+    const user = await User.create({
+        userid,
+        firstname,
+        secondname
+    })
+
+    const createdUser = await User.findById(user._id)
+
+    //check for user creation
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while User creation")
+    }
+
+    //return res if created 
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "User created successfully")
+    )
+})
+
+export const deleteUser = asyncHandler( async (req, res) => {
+    const userid = req.params['id']
+    const existedUser = await User.findOne({
+        $or: [{userid}]
+    })
+    //If User do not exist
+    if(!existedUser) {
+        throw new ApiError(409, "User do not exist. hence, no operation can be done")
+    }
+
+    //If User exist
+    const result = await User.deleteOne({userId: userid});
+
+    if (result.deletedCount > 0) {
+        console.log(`User with userId "${userid}" deleted.`);
+        return res.status(201).json(
+          new ApiResponse(200, existedUser, "User deleted successfully")
+        )
+    } else {
+        console.log(`unbale to delete user with userId: ${userid} from db`);
+        throw new ApiError(500, "Server side error")
+    }
+})
+
+export const updateUser = asyncHandler( async (req, res) => {
+    
+})
+
+export const addPicturesPicture = asyncHandler( async (req, res) => {
+    
+})
+
